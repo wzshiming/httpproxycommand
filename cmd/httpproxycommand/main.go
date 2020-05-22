7fe9a5df11e53960569e1aef54da186c7973540a
wzshiming <wzshiming@foxmail.com>
2020-05-22 11:23:07 +0800

    adjust

--- cmd/httpproxycommand/main.go
+++ cmd/httpproxycommand/main.go
@@ -5,14 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
-	"net/http/httptest"
 	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/wzshiming/commandproxy"
-	"github.com/wzshiming/httpproxy"
+	"github.com/wzshiming/httpproxycommand"
 )
 
 const defaults = `httpproxycommand will start an http proxy and add HTTP_PROXY and HTTPS_PROXY to environ. 
@@ -37,37 +34,11 @@ func main() {
 		return
 	}
 
-	ph := &httpproxy.ProxyHandler{
-		ProxyDial: func(ctx context.Context, _ string, address string) (net.Conn, error) {
-			host, port, err := net.SplitHostPort(address)
-			if err != nil {
-				return nil, err
-			}
-			m := map[byte]string{
-				'h': host,
-				'p': port,
-			}
-			p := make([]string, len(proxy))
-			copy(p, proxy)
-			for i := range p {
-				p[i] = commandproxy.ReplaceEscape(p[i], m)
-			}
-			log.Printf("Connect to %s with %q", address, strings.Join(p, " "))
-			cp := commandproxy.ProxyCommand(ctx, p[0], p[1:]...)
-			return cp.Stdio()
-		},
-	}
-
-	s := httptest.NewServer(ph)
-	cmd := exec.Command(command[0], command[1:]...)
-	env := append(os.Environ(), fmt.Sprintf("HTTP_PROXY=%s", s.URL), fmt.Sprintf("HTTPS_PROXY=%s", s.URL))
-	cmd.Env = env
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
 	log.Printf("Run command %q", strings.Join(command, " "))
-	err = cmd.Run()
+	err = httpproxycommand.ProxyCommand(context.Background(), proxy, command)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		fmt.Fprintf(os.Stderr, defaults)
+		flag.PrintDefaults()
 	}
 }
---
+++ proxy.go
@@ -0,0 +1,51 @@
+package httpproxycommand
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http/httptest"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/wzshiming/commandproxy"
+	"github.com/wzshiming/httpproxy"
+)
+
+type DialProxyCommand []string
+
+func (p *DialProxyCommand) DialContext(ctx context.Context, _ string, address string) (net.Conn, error) {
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+	m := map[byte]string{
+		'h': host,
+		'p': port,
+	}
+	proxy := make([]string, len(*p))
+	copy(proxy, *p)
+	for i := range proxy {
+		proxy[i] = commandproxy.ReplaceEscape(proxy[i], m)
+	}
+	log.Printf("Connect to %s with %q", address, strings.Join(proxy, " "))
+	cp := commandproxy.ProxyCommand(ctx, proxy[0], proxy[1:]...)
+	return cp.Stdio()
+}
+
+func ProxyCommand(ctx context.Context, proxy []string, command []string) error {
+	dp := DialProxyCommand(proxy)
+	s := httptest.NewServer(&httpproxy.ProxyHandler{
+		ProxyDial: dp.DialContext,
+	})
+	defer s.Close()
+	cmd := exec.CommandContext(ctx, command[0], command[1:]...)
+	env := append(os.Environ(), fmt.Sprintf("HTTP_PROXY=%s", s.URL), fmt.Sprintf("HTTPS_PROXY=%s", s.URL))
+	cmd.Env = env
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
